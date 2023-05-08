03cc33e09f7ce4cb5aa5ffb41fd64fb5d9a47dd1
Luis Alvarez Sanchez <asluisinfo@gmail.com>
2023-05-08 00:52:18 -0700

    Implementing the HistoryRepository interface using HistoryRepositoryImpl. This class describes how to extract the data from the database for each particular query.

--- src/main/java/com/example/CheckoutService/api/service/HistoryRepositoryImpl.java
+++ src/main/java/com/example/CheckoutService/api/service/HistoryRepositoryImpl.java
@@ -1,2 +1,30 @@
-package com.example.CheckoutService.api.service;public class HistoryRepositoryImpl {
+package com.example.CheckoutService.api.service;
+
+import com.example.CheckoutService.api.model.History;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.criteria.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HistoryRepositoryImpl implements HistoryRepository{
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<History> findBySellerID(String sellerID){
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+
+        CriteriaQuery<History> query = cb.createQuery(History.class);
+        Root<History> history = query.from(History.class);
+
+        Path<String> sIDPath = history.get("sellerID");
+
+        query.select(history).where(cb.equal(sIDPath, sellerID)); // is current sID equal to given sellerID?
+
+        return entityManager.createQuery(query).getResultList();
+    }
+
 }
