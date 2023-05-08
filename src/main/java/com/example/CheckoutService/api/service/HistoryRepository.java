98f9a414b327eaac15c1d594043dce0abf71c72c
Luis Alvarez Sanchez <asluisinfo@gmail.com>
2023-05-07 23:35:55 -0700

    Added prebuilt queries for getting history transactions.

--- src/main/java/com/example/CheckoutService/api/service/HistoryRepository.java
+++ src/main/java/com/example/CheckoutService/api/service/HistoryRepository.java
@@ -1,11 +1,23 @@
 package com.example.CheckoutService.api.service;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.example.CheckoutService.api.model.History;
 
+import java.util.List;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called HistoryRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface HistoryRepository extends CrudRepository<History, Integer>{
+
+    @Query("SELECT hist FROM history hist WHERE hist.sellerID = ?1")
+    List<History> findBySellerID(String sellerID);
+
+    @Query("SELECT hist FROM history hist WHERE hist.buyerID= ?1")
+    List<History> findByBuyerID(String buyerID);
+
+    @Query("SELECT hist FROM history hist WHERE hist.listingTitle LIKE %?1%")
+    List<History> findByListingTitle(String listingTitle);
 }
