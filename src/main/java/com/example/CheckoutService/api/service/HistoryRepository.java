dfc28a4109f7a9a9f9cbb39dd4ccc6e537c3c5a1
Luis Alvarez Sanchez <asluisinfo@gmail.com>
2023-05-08 00:51:22 -0700

    Modified HistoryRepository class to include general queries and turned it into a basic interface.

--- src/main/java/com/example/CheckoutService/api/service/HistoryRepository.java
+++ src/main/java/com/example/CheckoutService/api/service/HistoryRepository.java
@@ -1,5 +1,6 @@
 package com.example.CheckoutService.api.service;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.example.CheckoutService.api.model.History;
@@ -7,10 +8,8 @@ import com.example.CheckoutService.api.model.History;
 import java.util.List;
 
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called HistoryRepository
-// CRUD refers Create, Read, Update, Delete
-
-public interface HistoryRepository extends CrudRepository<History, Integer>{
+// Repo interface describes how to interact w/ DB using custom operations.
+public interface HistoryRepository{
 
     @Query("SELECT hist FROM history hist WHERE hist.sellerID = ?1")
     List<History> findBySellerID(String sellerID);
